afd80be2a63f6ba5a0bb6495bc28ff9a0ee71548
kelevra82 <tdmatte82@gmail.com>
2025-05-17 02:39:13 -0500

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function Weather() {
+  const [weatherData, setWeatherData] = useState({});
+  const API_KEY = process.env.API_KEY;
+
+  const lat = 51.5098;
+  const lon = -0.118;
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`
+        );
+        const data = await res.json();
+        setWeatherData(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
+
+  if (!weatherData.main) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <p>
+        <strong>Temp: </strong>
+        {weatherData.main.temp}°F
+      </p>
+    );
+  }
+}
